44d349c2081bb9a02b8f09e623b7e65ff745609e
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-05 20:13:39 -0600

    zoomIn-Out design n functionality

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit{
+
+  @ViewChild('map') divMap?: ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
+
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'This HTML element was not found';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+      minZoom: 2,
+      });
+
+      this.mapListeners();
+  }
+
+  mapListeners(){
+    if(!this.map) throw 'Map not init';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value : string ){
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
