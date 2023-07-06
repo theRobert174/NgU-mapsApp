aaef6417963c509ce4522a3e5e7e08eb9721da69
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-05 21:13:39 -0600

    dynamic markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
@@ -10,6 +15,8 @@ export class MarkersPageComponent implements AfterViewInit{
   public zoom: number = 10;
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-74.5, 40);
+
+  public markers: MarkerAndColor[] = [];
 
 
   ngAfterViewInit(): void {
@@ -24,7 +31,32 @@ export class MarkersPageComponent implements AfterViewInit{
       minZoom: 2,
       });
 
-      const marker = new Marker({ color: 'red'}).setLngLat(this.currentLngLat).addTo(this.map);
+      // const markerHTML = document.createElement('div');
+      // markerHTML.innerHTML = 'Rodrigo Martinez';
+      // const marker = new Marker({ color: 'red', element: markerHTML, }).setLngLat(this.currentLngLat).addTo(this.map);
+  }
+
+  createMarker(){
+    if(!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker( lngLat: LngLat, color: string = 'white') {
+    if(!this.map) return;
+
+    const marker = new Marker({
+      color,
+      draggable: true,
+    }).setLngLat(lngLat).addTo(this.map);
+
+    this.markers.push({color, marker});
+  }
+
+  deleteMarker(index : number){
+    this.markers[index].marker.remove();
+    this.markers.splice(index,1)
   }
 
 }
